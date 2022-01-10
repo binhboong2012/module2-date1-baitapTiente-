50ecb79048538f3a38ea181f0d809c3a5b80aab5
Nguyen Thanh Binh <kubap@MacBook-Pro-cua-KU.local>
2022-01-10 16:42:36 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,16 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số USD: ");
+        usd = scanner.nextDouble();
+        double rate = usd * vnd;
+        System.out.println("Giá trị VND: " + rate);
+    }
+}
